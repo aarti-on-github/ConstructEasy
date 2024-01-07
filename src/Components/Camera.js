83c6bbad10d2e0bcae85898716c59f3c0a5529dc
Aarti <aartimishra1209@gmail.com>
2024-01-07 12:26:53 +0530

    camerapage

--- src/Components/Camera.js
+++ src/Components/Camera.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Webcam from 'react-webcam';
 import { useRef,useState } from 'react';
+import {TextField} from '@mui/material'
+import Navbar from './Navbar';
 function Camera() {
     const webcamRef = useRef(null);
     const [capturedData, setCapturedData] = useState(null);
@@ -20,15 +22,18 @@ function Camera() {
 
   return (
     <div>
+       <Navbar/>
       <h1>Photos for Attendance</h1>
-      
+      <div className='text'>
+      <TextField id="outlined-basic"label="Worker Id" variant="outlined"  />
+      </div>
       <div className="WebcamContainer">
         <Webcam
           audio={false}
-          height={480}
+          height={250}
           ref={webcamRef}
           screenshotFormat="image/jpeg"
-          width={640}
+          width={250}
         />
       </div>
 
@@ -38,7 +43,7 @@ function Camera() {
 
       {capturedData && (
         <div className="CapturedImage">
-          <h2>Captured Image:</h2>
+          <h2 style={{marginBottom:'2rem'}}>Captured Image:</h2>
           <img src={capturedData.imageSrc} alt="Captured" style={{ maxWidth: '100%' }} />
           <p>Date: {capturedData.date}</p>
           <p>Time: {capturedData.time}</p>
