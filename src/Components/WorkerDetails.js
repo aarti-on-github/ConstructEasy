e63d31b0df0c648f60c5c297c34d76ae30dea6fe
Abhishek Upadhyay <96608611+megablazikenabhishek@users.noreply.github.com>
2024-01-15 12:08:28 +0000

    sgdfus

--- src/Components/WorkerDetails.js
+++ src/Components/WorkerDetails.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useEffect } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -7,48 +8,54 @@ import { CardActionArea } from '@mui/material';
 import { convertLength } from '@mui/material/styles/cssUtils';
 import Navbar from './Navbar';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useState } from 'react';
+import BACKEND_URL from '../backend';
 
 function WorkerDetails() {
+  const [workers, setWorkers] = useState([]);
+  useEffect(()=>{
+    const getWorkers = async () => {
+      try{
+        const res = await axios.get(`${BACKEND_URL}/api/getAllWorker`);
+        if(res.status === 200){
+          setWorkers(res.data);
+        }
+      }catch(err){
+        console.log(err);
+      }
+    }
+    getWorkers();
+  }, [])
+
+  console.log(workers);
+
   return (
     <div className='wd'>
       <Navbar/>
       <h2>Worker Details</h2>
-    <div className='workerdetails'>
-      <div class="profile-card">
-    <div class="profile-picture">
-      <img src="/AartiImage.jpg" alt="Profile Picture"/>
+      {workers.length === 0 ? <h1>Loading...</h1> :
+      workers.map((worker) => (
+        <div className='workerdetails' style={{marginBottom: "150px"}}>
+          <div class="profile-card">
+            <div class="profile-picture">
+              <img src={`${BACKEND_URL}/${worker.profile_pic}`} alt="Profile Picture"/>
+            </div>
+            <div class="name">{worker.name}</div>
+            <div class="info">Age:{worker.age}</div>
+            <div class="info">Salary: ${worker.salary}</div>
+            <div class="info">Phone: {worker.contact}</div>
+            <div class="info">Address: {worker.address}</div>
+            <div className='profile-buttons'>
+              <Link to='/updateWorker'>
+              <button onClick={()=> window.location.href=`/updateWorker/${worker._id}`}>Update</button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      ))
+      }
     </div>
-    <div class="name">Aarti Mishra</div>
-    <div class="info">Age:21</div>
-    <div class="info">Salary: $50,000</div>
-    <div class="info">Phone:+91 8104025348</div>
-    <div class="info">Address:C,404 Sai Garden </div>
-    <div className='profile-buttons'>
-      <Link to='/updateWorker'>
-      <button>Update</button>
-      </Link>
-    </div>
-  </div>
-    </div>
-    <div className='workerdetails'>
-      <div class="profile-card">
-    <div class="profile-picture">
-      <img src="/AartiImage.jpg" alt="Profile Picture"/>
-    </div>
-    <div class="name">Aarti Mishra</div>
-    <div class="info">Age: 21</div>
-    <div class="info">Salary: $50,000</div>
-    <div class="info">Phone : +91 8104025348</div>
-    <div class="info">Address: C,404 Sai Garden </div>
-    <div className='profile-buttons'>
-      <button>Update</button>
-    </div>
-  </div>
-    </div>
-    <div className='add'>
-    <button>Add New Worker</button>
-  </div>
-  </div>
   )
 }
 
